c6d5d57008f026599f880bd915a0a9fdf407f1a3
Mohiina <mohiinau@gmail.com>
2025-12-21 05:49:04 -0500

    Move backend files into backend folder and apply CORS fix

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const cors = require('cors');   
 const app = express();
 const apiRoutes = require('./api');
 const db = require('./db');
 
+app.use(cors());  
 app.use(express.json());
 app.use('/api', apiRoutes);
 
 const PORT = process.env.PORT || 3001;
+
+
 
 db.sync()
   .then(() => {
